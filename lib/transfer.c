fd7aa69f0a3748c823396ba46a47c3dcd5fb03e5
Sergey Markelov <sergey@solidstatenetworks.com>
2022-04-22 14:48:14 -0700

    SPTCH-2969: Fix rate limiting not applied globally.

--- lib/transfer.c
+++ lib/transfer.c
@@ -434,6 +434,7 @@ bool Curl_meets_timecondition(struct Curl_easy *data, time_t timeofdoc)
 static CURLcode readwrite_data(struct Curl_easy *data,
                                struct connectdata *conn,
                                struct SingleRequest *k,
+                               struct curltime *nowp,
                                int *didwhat, bool *done,
                                bool *comeback)
 {
@@ -465,6 +466,14 @@ static CURLcode readwrite_data(struct Curl_easy *data,
 #else
       FALSE;
 #endif
+
+    /* make sure not more than the max recv speed bytes downloaded at once */
+    if(data->set.max_recv_speed && data->set.max_recv_speed < buffersize) {
+      const curl_off_t toread = data->set.max_recv_speed -
+        (data->progress.downloaded - data->progress.dl_limit_size) %
+          data->set.max_recv_speed;
+      bytestoread = (size_t)toread;
+    }
 
     if(
 #ifdef USE_NGHTTP2
@@ -781,6 +790,14 @@ static CURLcode readwrite_data(struct Curl_easy *data,
       break;
     }
 
+    if(Curl_pgrsLimitWaitTime(data->progress.downloaded,
+                              data->progress.dl_limit_size,
+                              data->set.max_recv_speed,
+                              data->progress.dl_limit_start,
+                              *nowp)) {
+      maxloops = 0;
+    }
+
   } while(data_pending(data) && maxloops--);
 
   if(maxloops <= 0) {
@@ -845,6 +862,7 @@ static void win_update_buffer_size(curl_socket_t sockfd)
  */
 static CURLcode readwrite_upload(struct Curl_easy *data,
                                  struct connectdata *conn,
+                                 struct curltime *nowp,
                                  int *didwhat)
 {
   ssize_t i, si;
@@ -1068,6 +1086,13 @@ static CURLcode readwrite_upload(struct Curl_easy *data,
       }
     }
 
+    if(Curl_pgrsLimitWaitTime(data->progress.uploaded,
+                              data->progress.ul_limit_size,
+                              data->set.max_send_speed,
+                              data->progress.ul_limit_start,
+                              *nowp)) {
+      break;
+    }
 
   } while(0); /* just to break out from! */
 
@@ -1083,6 +1108,7 @@ static CURLcode readwrite_upload(struct Curl_easy *data,
  */
 CURLcode Curl_readwrite(struct connectdata *conn,
                         struct Curl_easy *data,
+                        struct curltime *nowp,
                         bool *done,
                         bool *comeback)
 {
@@ -1138,7 +1164,7 @@ CURLcode Curl_readwrite(struct connectdata *conn,
      the stream was rewound (in which case we have data in a
      buffer) */
   if((k->keepon & KEEP_RECV) && (select_res & CURL_CSELECT_IN)) {
-    result = readwrite_data(data, conn, k, &didwhat, done, comeback);
+    result = readwrite_data(data, conn, k, nowp, &didwhat, done, comeback);
     if(result || *done)
       goto out;
   }
@@ -1147,7 +1173,7 @@ CURLcode Curl_readwrite(struct connectdata *conn,
   if((k->keepon & KEEP_SEND) && (select_res & CURL_CSELECT_OUT)) {
     /* write */
 
-    result = readwrite_upload(data, conn, &didwhat);
+    result = readwrite_upload(data, conn, nowp, &didwhat);
     if(result)
       goto out;
   }
